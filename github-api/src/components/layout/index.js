e3a5e2580f3a752867c52788176983dd5a98ece0
JCastro456 <jackelinecastrocardoso@hotmail.com>
2022-06-02 14:55:34 -0300

    reactapi

---
+++ github-api/src/components/layout/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./styled";
+import Header from "../header";
+
+const Layout = ({ children }) => {
+    return (
+        <S.WrapperLayout>
+            <Header/>
+            {children}
+        </S.WrapperLayout>
+    );
+}
+
+export default Layout;
